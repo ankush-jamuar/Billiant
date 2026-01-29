ab69f2c947fb147e5009bc2b7e1efa0512346843
ankush <jamuar66@gmail.com>
2026-01-29 14:19:11 +0530

    frontend: added app layout nd also the protected routes

--- Frontend/src/app/routes.jsx
+++ Frontend/src/app/routes.jsx
@@ -1,19 +1,27 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom';
-import Dashboard from '../pages/dashboard/Dashboard';
-import Login from '../pages/auth/Login';
-import Register from '../pages/auth/Register';
-import VerifyEmail from '../pages/auth/VerifyEmail';
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "../pages/dashboard/Dashboard";
+import Login from "../pages/auth/Login";
+import Register from "../pages/auth/Register";
+import VerifyEmail from "../pages/auth/VerifyEmail";
+import ProtectedRoute from "../routes/ProtectedRoute";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/verify-email" element={<VerifyEmail />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
