fd795f9c8318a022cdca5ae9e2e558c5252bc570
ankush <jamuar66@gmail.com>
2026-03-16 14:55:55 +0530

    fix puppeteer for render

--- Backend/services/pdf.services.js
+++ Backend/services/pdf.services.js
@@ -2,7 +2,13 @@ import puppeteer from "puppeteer";
 
 export const generatePdfFromHtml = async (html) => {
   const browser = await puppeteer.launch({
-    headless: true,
+    headless: "new",
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-gpu"
+    ]
   });
 
   const page = await browser.newPage();
